014f30b4a852cefb11cca38dc86e8aeb841137d4
mahimagontalwar <70761344+mahimagontalwar@users.noreply.github.com>
2024-09-22 14:32:27 +0530

    Update Balance.js

--- src/Balance.js
+++ src/Balance.js
@@ -2,11 +2,7 @@ import React from 'react';
 
 function Balance({ transactions }) {
   //const totalBalance = transactions.reduce((acc, transaction) => acc + transaction.amount, 0);
- const totalBalance = transactions.reduce((acc, transaction) => {
-    return transaction.type === 'Income' 
-      ? acc + transaction.amount 
-      : acc - transaction.amount;
-  }, 0);
+  const totalBalance = transactions.reduce((acc, transaction) => acc + transaction.amount, 0);
   return (
     <div className="balance">
       <h2>Total Balance: ${totalBalance.toFixed(2)}</h2>
